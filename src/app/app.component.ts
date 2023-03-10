2fc6099c47399374d306aea5d8791f7a7eb68d17
IanZGray <ianzacharygray@gmail.com>
2023-03-09 23:32:28 -0500

    lots of changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,19 +31,40 @@ export class AppComponent {
   ${this.hourCalc(this.d.getHours())}:${this.d.getMinutes()}`
   currentAmPm = ` ${this.amPmCalc(this.d.getHours())}`
 
-  displayToggle:string = 'flex flex-wrap justify-evenly p-2 '
+  displayToggle:string = 'hidden'
+  appDisplay:string = 'hidden'
   appName:string = ''
-  buttonClass = 'flex self-center border-2 rounded-full pt-3 pl-3 pb-9 pr-9 text-2xl my-2 mx-1'
-  buttonBenchClass = 'flex self-center border-2 rounded-full pt-2 pl-2 pb-7 pr-7 text-xl mb-2 mx-1 bg-blue-600 '
+
+  iconColor:string = 'bg-yellow-600'
+  iconShape:string = 'rounded-full'
+
+  buttonClass:string = 'flex self-center border-2 pt-3 pl-3 pb-9 pr-9 text-2xl my-2 mx-1'
+  buttonBenchClass:string = 'flex self-center border-2 pt-2 pl-2 pb-7 pr-7 text-xl mb-2 mx-1 bg-blue-600 '
+  colorClass:string = 'flex self-center border-2 rounded-full p-4 text-2xl my-2 mx-1'
+
+  appDisplayClass:string = 'flex-wrap justify-center py-4 px-6 rounded-md drop-shadow-md'
+  appDisplayStyle:string = 'flex'
+  appHiddenStyle:string = 'none'
 
   buttonArray = ['heroUsers', 'heroCurrencyDollarSolid','heroEnvelopeSolid','heroGlobeAltSolid']
 
-  buttonArrayBench = ['heroChatBubbleLeftRightSolid','heroClockSolid','heroMagnifyingGlassPlus', 'ionColorPalette', 'ionImage']
+  buttonArrayBench = ['heroChatBubbleLeftRightSolid','heroClockSolid','heroMagnifyingGlassPlus', 'ionColorPalette', 'ionImage', 'heroMap']
 
 
-  operateWindow(name: string) {
+  testingArray: {icon:string, func:any}[] = [{ icon:'heroUsers', func:(id:string) => {
+    let elm:any = document.getElementById(id);
+    console.log(elm)
+    if(elm.className === 'appHiddenClass') {
+      elm.className = 'appDisplayClass'
+    } else {
+      elm.className = 'appHiddenClass'
+    }
+  } },]
+
+
+  operateWindow() {
     if (this.displayToggle === 'hidden') {
-      this.displayToggle = 'flex'
+      this.displayToggle = 'flex flex-wrap justify-start py-4 px-6'
     } else {
       this.displayToggle = 'hidden'
     }
@@ -57,4 +78,33 @@ export class AppComponent {
     this.buttonArray.push(name)
     this.buttonArrayBench.splice(index , 1)
   }
+
+  // updateIcons() {
+  //   if (this.appDisplay === 'hidden') {
+  //     this.appDisplay = 'flex flex-wrap justify-center py-4 px-6 rounded-md drop-shadow-md'
+  //   } else {
+  //     this.appDisplay = 'hidden'
+  //   }
+  // }
+
+  updateIconColor(color:string) {
+    this.iconColor = color
+  }
+
+  updateIconShape(shape:string) {
+    this.iconShape = shape
+  }
+
+  appWindowToggle(id:string) {
+    let elm:any = document.getElementById(id);
+    console.log(elm)
+    if(elm.style.display === this.appHiddenStyle) {
+
+      elm.style.display = this.appDisplayStyle
+    } else {
+
+      elm.style.display = this.appHiddenStyle
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { NgIconsModule } from '@ng-icons/core';
-import { heroUsers, heroPlus, heroSquaresPlus, heroMagnifyingGlassPlus, heroChartBar } from '@ng-icons/heroicons/outline';
+import { heroUsers, heroPlus, heroSquaresPlus, heroMagnifyingGlassPlus, heroChartBar, heroMap, heroXCircle,  } from '@ng-icons/heroicons/outline';
 import { heroCurrencyDollarSolid, heroEnvelopeSolid, heroGlobeAltSolid, heroChatBubbleLeftRightSolid, heroClockSolid } from '@ng-icons/heroicons/solid';
 import { jamBatteryThreeQuartersF } from '@ng-icons/jam-icons';
 import { ionColorPalette, ionImage } from '@ng-icons/ionicons';
@@ -17,7 +17,7 @@ import { ionColorPalette, ionImage } from '@ng-icons/ionicons';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgIconsModule.withIcons({ heroUsers, heroCurrencyDollarSolid, heroEnvelopeSolid, heroGlobeAltSolid, heroPlus, heroChatBubbleLeftRightSolid, heroClockSolid, heroSquaresPlus, heroMagnifyingGlassPlus,jamBatteryThreeQuartersF, heroChartBar, ionColorPalette, ionImage }),
+    NgIconsModule.withIcons({ heroUsers, heroCurrencyDollarSolid, heroEnvelopeSolid, heroGlobeAltSolid, heroPlus, heroChatBubbleLeftRightSolid, heroClockSolid, heroSquaresPlus, heroMagnifyingGlassPlus,jamBatteryThreeQuartersF, heroChartBar, ionColorPalette, ionImage, heroMap, heroXCircle, }),
   ],
   providers: [],
   bootstrap: [AppComponent]
